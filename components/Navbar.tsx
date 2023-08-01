00b83fd3e063a02006feae1a19753f2002496731
Artur.Nahaitsev <artur.nahaitsev@ukad-group.com>
2023-08-01 15:03:10 +0300

    implemented authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,18 @@
 import { NavLinks } from "@/constants";
+import { getCurrentUser } from "@/lib/session";
 import Image from "next/image";
 import Link from "next/link";
 import AuthProviders from "./AuthProviders";
+import ProfileMenu from "./ProfileMenu";
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
 
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
         <Link href="/">
-          <Image src="/logo.svg" width={115} height={43} alt="Flexibble" />
+          <Image src="/logo.svg" width={116} height={43} alt="Flexibble" />
         </Link>
         <ul className="xl:flex hidden text-small gap-7">
           {NavLinks.map((link) => (
@@ -22,9 +24,10 @@ const Navbar = () => {
       </div>
 
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
+            <ProfileMenu session={session} />
+
             <Link href="/create-project">Share Work</Link>
           </>
         ) : (
